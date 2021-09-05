32a116fa477400bbc715b4d9f8c385d32cfcbf25
cuvillier <cuvillier@gmail.com>
2021-09-05 18:33:40 +0200

    Issue #6: remove thingid=0

--- ingestor/src/main/java/com/lasrosas/iot/core/ingestor/sensors/api/ThingDataMessage.java
+++ ingestor/src/main/java/com/lasrosas/iot/core/ingestor/sensors/api/ThingDataMessage.java
@@ -1,27 +1,4 @@
 package com.lasrosas.iot.core.ingestor.sensors.api;
 
-import java.time.LocalDateTime;
-
-import com.lasrosas.iot.core.shared.telemetry.NotPartOfState;
-
 public class ThingDataMessage {
-
-	@NotPartOfState
-	private long thingid;
-
-	@NotPartOfState
-	private LocalDateTime time;
-
-	public long getThingid() {
-		return thingid;
-	}
-	public void setThingid(long thingid) {
-		this.thingid = thingid;
-	}
-	public LocalDateTime getTime() {
-		return time;
-	}
-	public void setTime(LocalDateTime time) {
-		this.time = time;
-	}
 }
